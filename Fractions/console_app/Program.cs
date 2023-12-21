a965aa72283b66c6eb5d7ee2b5aaba8e056862c2
pmad@techcollege.dk <pmad@techcollege.dk>
2023-12-21 11:15:13 +0100

    work, add QuadraticFormula

--- Fractions/console_app/Program.cs
+++ Fractions/console_app/Program.cs
@@ -1,5 +1,9 @@
 ﻿using techmath;
 
+/***********************/
+// Fraction part       */
+/***********************/
+
 Fraction f1 = new(-3, 4);
 Console.WriteLine("f1: " + f1);
 
@@ -11,7 +15,55 @@ Console.WriteLine("f1.Multiply(f2): " + f1.Multiply(f2));
 Console.WriteLine("f1.Minus(f2):    " + f1.Minus(f2));
 Console.WriteLine("f1.Add(f2):      " + f1.Add(f2));
 
+Console.WriteLine();
+
+/***********************/
+// Shape part          */
+/***********************/
+
+/***********************/
+// QuadraticFormula    */
+/***********************/
+
+
+QuadraticFormula qf;
+
+int a = 1, b = 2, c = -1;
+
+qf = new QuadraticFormula(a, b, c);
+
+
+Console.WriteLine("A: " + a);
+Console.WriteLine("B: " + b);
+Console.WriteLine("C: " + c);
+
+
+Console.WriteLine("D: " + qf.getD().ToString());
+if (qf.getD() < 0)
+{
+    Console.WriteLine("S: " + qf.getS().ToString());
+    Console.WriteLine("T: " + qf.getT().ToString());
+}
+
+else if (qf.getD() == 0)
+{
+    Console.WriteLine("X: " + qf.getX1().ToString());
+    Console.WriteLine("S: " + qf.getS().ToString());
+    Console.WriteLine("T: " + qf.getT().ToString());
+}
+else if (qf.getD() > 0)
+{
+    Console.WriteLine("Kvardratrod D: " + qf.getSqrtD().ToString());
+    Console.WriteLine("X1: " + qf.getX1().ToString());
+    Console.WriteLine("X2: " + qf.getX2().ToString());
+    Console.WriteLine("S: " + qf.getS().ToString());
+    Console.WriteLine("T: " + qf.getT().ToString());
+}
+
+Console.WriteLine();
+
+
 #if DEBUG
-  Console.ReadLine();
+Console.ReadLine();
 #endif
 
---
+++ Fractions/math/QuadraticFormula.cs
@@ -0,0 +1,95 @@
+﻿// The core part of the system.
+// Contains:
+//      - few custom exception classes
+//      - the main class QuadraticFormula
+
+using System;
+
+namespace techmath
+{
+    [Serializable]
+    public class NegativeDException : Exception
+    {
+        public NegativeDException() : base("D is negative") { }
+    }
+
+    [Serializable]
+    public class ZeroAException : Exception
+    {
+        public ZeroAException() : base("A is zero") { }
+    }
+
+    public class QuadraticFormula
+    {
+        private int a;
+
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                if (value == 0)
+                    throw new ZeroAException();
+                else
+                    a = value;
+            }
+        }
+
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public QuadraticFormula()
+        {
+            A = 1;
+            B = 1;
+            C = 1;
+        }
+
+        public QuadraticFormula(int _a, int _b, int _c)
+        {
+            A = _a;
+            B = _b;
+            C = _c;
+        }
+
+        public double getD()
+        {
+            return B * B - 4 * A * C;
+        }
+
+        public double getSqrtD()
+        {
+           if (getD() < 0)
+                throw new NegativeDException();
+
+            return Math.Sqrt(getD());
+        }
+
+        public double getS()
+        {
+            return -B / (2 * A);
+        }
+
+        public double getT()
+        {
+            return -getD() / (4 * A);
+        }
+
+        public double getX1()
+        {
+            if (getD() < 0)
+                throw new NegativeDException();
+            return (-B + getSqrtD()) / (2 * A);
+        }
+
+        public double getX2()
+        {
+            if (getD() < 0)
+                throw new NegativeDException();
+            return (-B - getSqrtD()) / (2 * A);
+        }
+    }
+}
