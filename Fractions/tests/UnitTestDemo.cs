26fce67129ec61449fd18cf9eaf4b3e78ab40420
pmad@techcollege.dk <pmad@techcollege.dk>
2024-05-02 10:50:19 +0200

    Add theory sample

--- Fractions/tests/UnitTestDemo.cs
+++ Fractions/tests/UnitTestDemo.cs
@@ -60,4 +60,18 @@ public class DemoUnitTest
         Assert.Single(stack);
     }
 
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(-4, -6, -10)]
+    [InlineData(-2, 2, 0)]
+    [InlineData(int.MinValue, -1, int.MaxValue)]
+    public void CanAddTheory(int value1, int value2, int expected)
+    {
+
+        var result = value1 + value2;
+
+        Assert.Equal(expected, result);
+    }
+
+
 }
